4d58364df1c2164c7874d874d580af6500e9fb8e
Nishmaa93 <nishmadahal01234@gmail.com>
2024-05-15 10:46:21 +0545

    login page

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-
+import '../screen/login_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-    
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'KaamKuro',
+            style: TextStyle(
+                fontSize: 35, color: Colors.brown, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter Email',
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (String value) {},
+                    validator: (value) {
+                      return value!.isEmpty ? 'Please enter email' : null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter Password',
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (String value) {},
+                    validator: (value) {
+                      return value!.isEmpty ? 'Please enter password' : null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Login'),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(255, 29, 191, 115),
+                              textStyle: TextStyle(color: Colors.white))))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
