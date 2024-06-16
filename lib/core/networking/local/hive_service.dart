69b13cc5e773c39b5d920e9e68d59655c24d77a8
Nishmaa93 <nishmadahal01234@gmail.com>
2024-06-16 21:03:34 +0545

    hive login

--- lib/core/networking/local/hive_service.dart
+++ lib/core/networking/local/hive_service.dart
@@ -21,11 +21,11 @@ class HiveService {
   }
 
   //Login
-  // Future<AuthHiveModel?> login(String email, String password) async {
-  //   var box = await Hive.openBox<AuthHiveModel>(HiveTableConstant.userBox);
-  //   var user = box.values.firstWhere(
-  //       (element) => element.email == email && element.password == password);
-  //   box.close();
-  //   return user;
-  // }
+  Future<AuthHiveModel?> login(String email, String password) async {
+    var box = await Hive.openBox<AuthHiveModel>(HiveTableConstant.userBox);
+    var user = box.values.firstWhere(
+        (element) => element.email == email && element.password == password);
+    box.close();
+    return user;
+  }
 }
