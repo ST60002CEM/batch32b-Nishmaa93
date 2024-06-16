3c65eb8adfcfccd316aa49247ced6fa2f2d52228
Nishmaa93 <nishmadahal01234@gmail.com>
2024-06-16 21:02:46 +0545

    added

--- lib/core/common/my_snackbar.dart
+++ lib/core/common/my_snackbar.dart
@@ -1,16 +1,16 @@
-// import 'package:flutter/material.dart';
-// import 'package:kaamkuro/app/navigator_key/navigator_key.dart';
+import 'package:flutter/material.dart';
+import 'package:kaamkuro/app/navigator_key/navigator_key.dart';
 
-// showMySnackBar({
-//   required String message,
-//   Color? color
-// }){
-//   ScaffoldMessenger.of(AppNavigator.navigatorKey.currentState!.context,).showSnackBar(
-//     SnackBar(
-//       content: Text(message),
-//       backgroundColor: color ?? Colors.green,
-//       duration: const Duration(seconds: 2),
-//       behavior: SnackBarBehavior.floating,
-//     ),
-//   );
-// }
+showMySnackBar({
+  required String message,
+  Color? color
+}){
+  ScaffoldMessenger.of(AppNavigator.navigatorKey.currentState!.context,).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      backgroundColor: color ?? Colors.green,
+      duration: const Duration(seconds: 2),
+      behavior: SnackBarBehavior.floating,
+    ),
+  );
+}
