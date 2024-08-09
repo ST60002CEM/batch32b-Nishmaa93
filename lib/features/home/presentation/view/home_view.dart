23500ca923b7290585d4184946e02e93661e98d4
Nishmaa93 <nishmadahal01234@gmail.com>
2024-08-09 12:38:11 +0545

    Home Page

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,165 +1,285 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:kaamkuro/app/constants/api_endpoint.dart';
-// import 'package:kaamkuro/features/job/presentation/viewmodel/job_viewmodel.dart';
- 
-// class HomeView extends ConsumerStatefulWidget {
-//   const HomeView({super.key});
- 
-//   @override
-//   ConsumerState<ConsumerStatefulWidget> createState() => _HomeViewState();
-// }
- 
-// class _HomeViewState extends ConsumerState<HomeView> {
-//   final ScrollController _scrollController = ScrollController();
- 
-//   @override
-//   void dispose() {
-//     _scrollController.dispose();
-//     super.dispose();
-//   }
- 
-//   @override
-//   Widget build(BuildContext context) {
-//     final state = ref.watch(jobViewModelProvider);
-//     print(state.lstJobs);
-//     return NotificationListener(
-//       onNotification: (notification) {
-//         if (notification is ScrollEndNotification) {
-//           // Scroll garda feri last ma ho ki haina bhanera check garne ani data call garne
-//           if (_scrollController.position.extentAfter == 0) {
-//             // Data fetch gara api bata
-//             ref.read(jobViewModelProvider.notifier).getAllJobs();
-//           }
-//         }
-//         return true;
-//       },
-//       child: RefreshIndicator(
-//         // Yo chai pull to refresh ko lagi ho
-//         color: Colors.green,
-//         backgroundColor: Colors.amberAccent,
-//         onRefresh: () async {
-//           ref.read(jobViewModelProvider.notifier).resetState();
-//         },
-//         child: Column(
-//           children: [
-//             Expanded(
-//               child: ListView.separated(
-//                 separatorBuilder: (context, index) => const Divider(),
-//                 controller: _scrollController,
-//                 itemCount: state.lstJobs.length,
-//                 physics: const AlwaysScrollableScrollPhysics(),
-//                 itemBuilder: (context, index) {
-//                   final job = state.lstJobs[index];
-//                   return ListTile(
-//                     leading:
-//                         Image.network(ApiEndpoints.jobImageUrl + job.imageUrl!),
-//                     title: Text(job.name!),
-//                     // subtitle: Text(job.email),
-//                   );
-//                 },
-//               ),
-//             ),
-//             // Data load huda feri progress bar dekhaune natra banda garne
-//             if (state.isLoading)
-//               const CircularProgressIndicator(color: Colors.red),
- 
-//             const SizedBox(height: 10),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kaamkuro/app/constants/api_endpoint.dart';
 import 'package:kaamkuro/features/job/presentation/viewmodel/job_viewmodel.dart';
- 
+
 class HomeView extends ConsumerStatefulWidget {
   const HomeView({super.key});
- 
+
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _HomeViewState();
 }
- 
+
 class _HomeViewState extends ConsumerState<HomeView> {
-  final ScrollController _scrollController = ScrollController();
- 
+  final ScrollController _horizontalScrollController1 = ScrollController();
+  final ScrollController _horizontalScrollController2 = ScrollController();
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   void dispose() {
-    _scrollController.dispose();
+    _horizontalScrollController1.dispose();
+    _horizontalScrollController2.dispose();
+    _searchController.dispose();
     super.dispose();
   }
- 
+
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(jobViewModelProvider);
-    print(state.lstJobs);
-    return NotificationListener(
-      onNotification: (notification) {
-        if (notification is ScrollEndNotification) {
-          if (_scrollController.position.extentAfter == 0) {
+
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Home'),
+          backgroundColor: Colors.brown,
+          automaticallyImplyLeading: false),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (notification) {
+          if (notification is ScrollEndNotification &&
+              _horizontalScrollController1.position.extentAfter == 0) {
             ref.read(jobViewModelProvider.notifier).getAllJobs();
           }
-        }
-        return true;
-      },
-      child: RefreshIndicator(
-        color: Colors.green,
-        backgroundColor: Colors.amberAccent,
-        onRefresh: () async {
-          ref.read(jobViewModelProvider.notifier).resetState();
+          return true;
         },
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.separated(
-                separatorBuilder: (context, index) => const Divider(),
-                controller: _scrollController,
-                itemCount: state.lstJobs.length,
-                physics: const AlwaysScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  final job = state.lstJobs[index];
-                  return ListTile(
-                    leading: job.imageUrl != null && job.imageUrl!.isNotEmpty
-                        ? Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
+        child: RefreshIndicator(
+          color: Colors.brown,
+          backgroundColor: Colors.white,
+          onRefresh: () async {
+            ref.read(jobViewModelProvider.notifier).resetState();
+          },
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Search Bar
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.search),
+                        hintText: 'Search for jobs...',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                      ),
+                      onChanged: (query) {
+                        // Trigger a search/filtering function when the user types
+                        // ref
+                        //     .read(jobViewModelProvider.notifier)
+                        //     .filterJobs(query);
+                      },
+                    ),
+                  ),
+                  // First list heading
+                  const Text(
+                    "Available Jobs",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 250,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      controller: _horizontalScrollController1,
+                      itemCount: state.lstJobs.length,
+                      itemBuilder: (context, index) {
+                        final job = state.lstJobs[index];
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 10.0),
+                          child: Card(
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
                             ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.network(
-                                ApiEndpoints.jobImageUrl + job.imageUrl!,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return const Icon(Icons.person);
-                                },
-                                loadingBuilder:
-                                    (context, child, loadingProgress) {
-                                  if (loadingProgress == null) {
-                                    return child;
-                                  }
-                                  return const Center(
-                                    child: CircularProgressIndicator(),
-                                  );
-                                },
+                            child: SizedBox(
+                              width: 180,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  job.imageUrl != null &&
+                                          job.imageUrl!.isNotEmpty
+                                      ? Container(
+                                          width: double.infinity,
+                                          height: 120,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                          ),
+                                          child: ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                            child: Image.network(
+                                              ApiEndpoints.jobImageUrl +
+                                                  job.imageUrl!,
+                                              fit: BoxFit.cover,
+                                              errorBuilder:
+                                                  (context, error, stackTrace) {
+                                                return const Icon(Icons.person,
+                                                    size: 50);
+                                              },
+                                              loadingBuilder: (context, child,
+                                                  loadingProgress) {
+                                                if (loadingProgress == null) {
+                                                  return child;
+                                                }
+                                                return const Center(
+                                                  child:
+                                                      CircularProgressIndicator(),
+                                                );
+                                              },
+                                            ),
+                                          ),
+                                        )
+                                      : const Icon(Icons.person, size: 50),
+                                  const SizedBox(height: 10),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8.0),
+                                    child: Text(
+                                      job.name!,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8.0),
+                                    child: const Text(
+                                      "Job Description",
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                          )
-                        : const Icon(Icons.person),
-                    title: Text(job.name!),
-                  );
-                },
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 20), // Space between the two lists
+                  // Second list heading
+                  const Text(
+                    "New Jobs",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 250,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      controller: _horizontalScrollController2,
+                      itemCount: state.lstJobs.length,
+                      itemBuilder: (context, index) {
+                        final job = state.lstJobs[index];
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 10.0),
+                          child: Card(
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: SizedBox(
+                              width: 180,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  job.imageUrl != null &&
+                                          job.imageUrl!.isNotEmpty
+                                      ? Container(
+                                          width: double.infinity,
+                                          height: 120,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                          ),
+                                          child: ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                            child: Image.network(
+                                              ApiEndpoints.jobImageUrl +
+                                                  job.imageUrl!,
+                                              fit: BoxFit.cover,
+                                              errorBuilder:
+                                                  (context, error, stackTrace) {
+                                                return const Icon(Icons.person,
+                                                    size: 50);
+                                              },
+                                              loadingBuilder: (context, child,
+                                                  loadingProgress) {
+                                                if (loadingProgress == null) {
+                                                  return child;
+                                                }
+                                                return const Center(
+                                                  child:
+                                                      CircularProgressIndicator(),
+                                                );
+                                              },
+                                            ),
+                                          ),
+                                        )
+                                      : const Icon(Icons.person, size: 50),
+                                  const SizedBox(height: 10),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8.0),
+                                    child: Text(
+                                      job.name!,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8.0),
+                                    child: const Text(
+                                      "Job Description",
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  if (state.isLoading)
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: CircularProgressIndicator(color: Colors.red),
+                    ),
+                ],
               ),
             ),
-            if (state.isLoading)
-              const CircularProgressIndicator(color: Colors.red),
-            const SizedBox(height: 10),
-          ],
+          ),
         ),
       ),
     );
